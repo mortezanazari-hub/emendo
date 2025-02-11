e915283aa0da0b9154906099a3b1b543ddb17e6c
AmirHossein <amirsolgi56@gmail.com>
2025-02-11 17:52:33 +0330

    تنظیمات مربوط به چندزبانه کردن اپلیکیشن

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:emendo/core/configs/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'features/intro/presentations/screens/intro_screen.dart';
+
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,6 +12,14 @@ Future<void> main() async{
     Builder(
       builder: (context) {
         return MaterialApp(
+          localizationsDelegates: [
+            AppLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+          ],
+          supportedLocales: AppLocalizations.supportedLocales,
+          locale: const Locale('en', 'US'),
           theme: AppTheme.lightTheme,
           darkTheme: AppTheme.darkTheme,
           debugShowCheckedModeBanner: false,
