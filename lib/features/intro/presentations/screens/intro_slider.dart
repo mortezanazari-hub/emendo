62c1ca4124a5a990f8d25f23f5b2f63b902194ff
Morteza Nazari <namo352@gmail.com>
2025-02-12 12:50:38 +0330

    یه بخشی از صفحه اسلایدر اینترو رو زدم.

--- lib/features/intro/presentations/screens/intro_slider.dart
+++ lib/features/intro/presentations/screens/intro_slider.dart
@@ -1,3 +1,5 @@
+import 'package:emendo/common/helper/is_dark_mode.dart';
+import 'package:emendo/core/configs/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class IntroSlider extends StatefulWidget {
@@ -10,6 +12,39 @@ class IntroSlider extends StatefulWidget {
 class _IntroSliderState extends State<IntroSlider> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: LayoutBuilder(builder: (context, constraints) {
+        return Column(
+          children: [
+            Container(
+              height: constraints.maxHeight * 0.55,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: context.isDarkMode
+                    ? DarkColors.bgColor
+                    : LightColors.bgColor,
+              ),
+              child: Image.asset(
+                "assets/images/png/intro_screen_slide1.png",
+                fit: BoxFit.fitWidth,
+                alignment: Alignment.topCenter,
+                isAntiAlias: true,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: constraints.maxHeight * 0.05),
+              child: Column(
+                children: [
+                  Text(
+                    "Task Management",
+                  ),
+                ],
+              ),
+            )
+          ],
+        );
+      }),
+    );
   }
 }
