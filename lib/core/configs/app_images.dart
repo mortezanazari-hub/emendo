93e3abb9d01afac7be3be07007d111e7e13afc0d
AmirHossein <amirsolgi56@gmail.com>
2025-02-17 16:54:31 +0330

    little change in AppImages class

--- lib/core/configs/app_images.dart
+++ lib/core/configs/app_images.dart
@@ -1,8 +1,10 @@
 class AppImages {
-  static const String getStartScreen = "assets/images/get_start_screen.png";
-  static const String logo = "assets/images/logo.png";
-  static const String miniLogo = "assets/images/mini_logo.png";
-  static const String nextButtonIntro = "assets/images/next_button_intro.svg";
+  static const String basePath = "assets/images/";
+
+  static const String getStartScreen = "${basePath}get_start_screen.png";
+  static const String logo = "${basePath}logo.png";
+  static const String miniLogo = "${basePath}mini_logo.png";
+  static const String nextButtonIntro = "${basePath}next_button_intro.svg";
 
   static String introScreenSlide(int index) {
     return "assets/images/intro_screen_slide${index.toString()}.png";
