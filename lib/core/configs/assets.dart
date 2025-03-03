3be77807a19910eef704cf51906fde9fe5f00559
AmirHossein <amirsolgi56@gmail.com>
2025-03-03 13:13:39 +0330

    Add bottomNavIcons Assets

--- lib/core/configs/assets.dart
+++ lib/core/configs/assets.dart
@@ -7,7 +7,10 @@ class Assets {
   static const String backButtonDark = "${basePath}back_button_dark.svg";
   static const String authCirclesView = "${basePath}auth_circles_view.svg";
   static const String googleLogo = "${basePath}google_logo.svg";
-
+  static const String notificationTab = "${basePath}notification_tab.svg";
+  static const String folderTab = "${basePath}folder_tab.svg";
+  static const String homeTab = "${basePath}home_tab.svg";
+  static const String profileTab = "${basePath}profile_tab.svg";
   static String introScreenSlide(int index) {
     return "assets/images/intro_screen_slide${index.toString()}.png";
   }
